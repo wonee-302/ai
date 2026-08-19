0c9a1bf969b4d0a1ed5cb842b0c427ebdcb5bfa2
wonee-302 <rakyungwon514@gmail.com>
2026-08-19 18:01:49 +0900

    0819

---
+++ source/04_DBMS/0820_03_JOIN.sql
@@ -0,0 +1,14 @@
+-- [III] JOIN :2개 이상의 테이블을 연결하여 데이터를 검색하는 방법 
+SELECT * FROM EMP WHERE ENAME='SCOTT'; --1행
+SELECT * FROM DEPT; -- 4행
+-- CROSS JOIN (FROM절에 테이블을 2개이상)
+SELECT * FROM EMP, DEPT  WHERE ENAME ='SCOTT'; -- EMP의 1행 * DEPT의 4행
+
+
+-- ▶ 1. EQUI JOIN: 공통필드 값이 일치되는 조건만 JOIN 
+SELECT * FROM EMP, DEPT  WHERE EMP.DEPTNO=DEPT.DEPTNO; 
+SELECT E.*, DNAME, LOC
+    FROM EMP E, DEPT D
+    WHERE E.DEPTNO=D.DEPTNO; 
+
+
